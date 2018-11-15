57a198aaf429f985f5b57c72e67b451dabe44762
franklyn034 <lewislulu@yahoo.com>
2018-11-15 09:50:09 +0100

    Implemented POST /api/books endpoint

--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const _ = require ('lodash');
 
-const {body} = require('./models/book')
+const {Book} = require('./models/book')
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/api/books',(req, res)=>{
+    const body = _.pick(req.body, ['title','author','publisher','year']);
+    const book = new Book(body);
+    book.save().then((book)=>{
+        res.status(200).send(book);
+    });
+});
 
 
 app.listen(port, ()=>{
